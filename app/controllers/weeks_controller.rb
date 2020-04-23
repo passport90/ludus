901e5949c2c0a25b767cc1f098b6a15a6af1ccec
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2020-04-23 16:12:50 +1000

    weeks styling

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -6,8 +6,16 @@ class WeeksController < ApplicationController
       Game.order(release_date: :desc).limit(1)
           .pluck("extract(isoyear from release_date)").first.to_i
     end
-    @weeks = Game.order(week: :asc).distinct
-                 .where("extract(year from release_date) = ?", @year)
-                 .pluck("extract(week from release_date) as week").map(&:to_i)
+    weeks_nums = Game.order(week: :asc).distinct
+                .where("extract(year from release_date) = ?", @year)
+                .pluck("extract(week from release_date) as week").map(&:to_i)
+    @weeks = weeks_nums.map do |week_num|
+      {
+        week_num: week_num,
+        week_start: (
+          Date.strptime(@year.to_s + week_num.to_s.rjust(2, '0'), '%G%V')
+        )
+      }
+    end
   end
 end
