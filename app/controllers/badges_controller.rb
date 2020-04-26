1622d45f97d7add33793fc118d895d3ce75bf24b
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2020-04-27 05:36:43 +1000

    badge edit

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -20,6 +20,17 @@ class BadgesController < ApplicationController
     redirect_to badges_path
   end
 
+  def edit
+    @badge = Badge.find(params[:id])
+  end
+
+  def update
+    @badge = Badge.find(params[:id])
+
+    @badge.update(badge_params)
+    redirect_to badge_path(@badge)
+  end
+
 private
   def badge_params
     params.require(:badge).permit(:name, :symbol, :award_date)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :badges_games, only: [:new, :create]
   resources :weeks, only: [:index]
   resources :years, only: [:index]
-  resources :badges, only: [:index, :show, :new, :create]
+  resources :badges, only: [:index, :show, :new, :create, :edit, :update]
   resources :games, only: [:index, :show, :new, :create, :edit, :update]
   resources :esrb_ratings, only: [:index, :new, :create]
   resources :genres, only: [:index, :show, :new, :create]
