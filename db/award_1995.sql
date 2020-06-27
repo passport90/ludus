dd8f736fcfe7e86dcc3b6a8e13fb9e9f5b1ea9c9
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2020-06-28 08:30:15 +1000

    1995 award

--- db/award_1995.sql
+++ db/award_1995.sql
@@ -3,9 +3,45 @@ select genres.name, count(*)
 from games
 join genres on games.genre_id = genres.id
 where games.release_date < '1996-01-01'
-and games.score >= '75'
 group by genres.name
 order by genres.name;
+
+
+-- 1995 Fighting Game of the Year --
+select title, platforms.name as platform, genres.name as genre, score
+from games
+join platforms on games.platform_id = platforms.id
+join genres on games.genre_id = genres.id
+where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
+and score >= '74'
+and genres.path <@ 'action.fighting'::ltree
+order by score desc, title, platforms.release_date
+limit 10;
+
+-- 1995 Shooter of the Year --
+select title, platforms.name as platform, genres.name as genre, score
+from games
+join platforms on games.platform_id = platforms.id
+join genres on games.genre_id = genres.id
+where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
+and score >= '74'
+and genres.path <@ 'action.shooter'::ltree
+order by score desc, title, platforms.release_date
+limit 10;
+
+-- 1995 Racing of the Year --
+select title, platforms.name as platform, genres.name as genre, score
+from games
+join platforms on games.platform_id = platforms.id
+join genres on games.genre_id = genres.id
+where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
+and score >= '74'
+and genres.path <@ 'racing'::ltree
+order by score desc, title, platforms.release_date
+limit 10;
 
 -- 1995 Platformer Game of the Year --
 select title, platforms.name as platform, genres.name as genre, score
@@ -18,15 +54,15 @@ and genres.path <@ 'action.platformer'::ltree
 order by score desc, title, platforms.release_date
 limit 10;
 
--- 1995 Non-platformer Action Game of the Year --
+-- 1995 Simulation Game of the Year --
 select title, platforms.name as platform, genres.name as genre, score
 from games
 join platforms on games.platform_id = platforms.id
 join genres on games.genre_id = genres.id
 where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
 and score >= '74'
-and genres.path <@ 'action'::ltree
-and not genres.path <@ 'action.platformer'::ltree
+and genres.path <@ 'simulation'::ltree
 order by score desc, title, platforms.release_date
 limit 10;
 
@@ -41,15 +77,33 @@ and genres.path <@ 'sports'::ltree
 order by score desc, title, platforms.release_date
 limit 10;
 
--- 1995 Non-Action-and-Sports Game of the Year --
+-- 1995 Strategy Game of the Year --
 select title, platforms.name as platform, genres.name as genre, score
 from games
 join platforms on games.platform_id = platforms.id
 join genres on games.genre_id = genres.id
 where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
 and score >= '74'
-and not genres.path <@ 'action'::ltree
+and genres.path <@ 'strategy'::ltree
+order by score desc, title, platforms.release_date
+limit 10;
+
+-- 1995 Miscellaneous Game of the Year --
+select title, platforms.name as platform, genres.name as genre, score
+from games
+join platforms on games.platform_id = platforms.id
+join genres on games.genre_id = genres.id
+where games.release_date < '1996-01-01'
+and games.release_date >= '1995-01-01'
+and score >= '74'
+and not genres.path <@ 'action.fighting'::ltree
+and not genres.path <@ 'action.platformer'::ltree
+and not genres.path <@ 'action.shooter'::ltree
+and not genres.path <@ 'racing'::ltree
+and not genres.path <@ 'simulation'::ltree
 and not genres.path <@ 'sports'::ltree
+and not genres.path <@ 'strategy'::ltree
 order by score desc, title, platforms.release_date
 limit 10;
 
