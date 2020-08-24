0de5685f01eea9a2caac0dd63772a9c2efd79a54
Marhadiasha Kusumawardhana <marhadiasak@icloud.com>
2020-08-24 18:03:56 +1000

    restyling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   def index
+    @disable_nav = true
   end
 
   def search
-    if params[:q] and params[:q].length >= 3
+    if params[:q] && (params[:q].length >= 3)
       @games = Game.where("title ilike '#{params[:q]}%'")
                    .order(release_date: :desc, title: :asc).all
     end
